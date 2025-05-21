57c4aa1a0599822411d7e4f45ceb167166a4b883
DequElite <maxrognov23@gmail.com>
2025-05-21 11:38:42 +0000

    test: add PasswordInput test

--- src/components/Input/PasswordInout.spec.tsx
+++ src/components/Input/PasswordInout.spec.tsx
@@ -1,9 +1,31 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import {describe, test, expect, vi} from 'vitest';
 import '@testing-library/jest-dom';
+import PasswordInput from './PasswordInput.component';
 
 describe('PasswordInput component testing', ()=>{
-    test('should be rendered', ()=>{
-        //todo: напиши тесты для PasswordInput component: нажатие на глазик и чек поменялся ли тип и т.д., т.п.
+    test('should be rendered with password type and toggles visibility by tipe', ()=>{
+        render(
+            <PasswordInput
+                title="Password"
+                placeholder="Enter password"
+                isRequired={true}
+                isError={false}
+                errorText=""
+            />
+        );
+
+        const input = screen.getByPlaceholderText('Enter password');
+        const eyeBtn = screen.getByRole('button');
+
+        expect(screen.getByText('Password')).toBeInTheDocument();
+        expect(input).toHaveAttribute('type', 'password');
+        expect(eyeBtn).toBeInTheDocument();
+
+        fireEvent.click(eyeBtn);
+        expect(input).toHaveAttribute('type', 'text');
+
+        fireEvent.click(eyeBtn);
+        expect(input).toHaveAttribute('type', 'password');
     });
 });
